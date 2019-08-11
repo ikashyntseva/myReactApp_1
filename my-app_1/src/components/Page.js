db0158ee7be0e18c86e70b3db0de053b06accf43
Irina Kashyntseva Personal <i.kashyntseva@gmail.com>
2019-08-11 13:35:18 +0200

    Actions

--- my-app_1/src/components/Page.js
+++ my-app_1/src/components/Page.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 export class Page extends React.Component {
   onYearBtnClick = e => {
     const year = +e.currentTarget.innerText
-    this.props.setYear(year)
+    this.props.getPhotos(year)
   }
   render() {
-    const { year, photos } = this.props
+    const { year, photos, isFetching } = this.props
     return (
       <div className="ib page">
         <p>
@@ -31,7 +31,8 @@ export class Page extends React.Component {
           </button>
         </p>
         <h3>{year} год</h3>
-        <p>У тебя {photos.length} фото.</p>
+        {/* добавили отрисовку по условию */}
+        {isFetching ? <p>Загрузка...</p> : <p>У тебя {photos.length} фото.</p>}
       </div>
     )
   }
@@ -40,5 +41,6 @@ export class Page extends React.Component {
 Page.propTypes = {
   year: PropTypes.number.isRequired,
   photos: PropTypes.array.isRequired,
-  setYear: PropTypes.func.isRequired, // добавили новое свойство в propTypes
+  getPhotos: PropTypes.func.isRequired, // добавили новое свойство в propTypes
+  isFetching: PropTypes.bool.isRequired,
 }
--- my-app_1/src/containers/App.js
+++ my-app_1/src/containers/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { User } from '../components/User'
 import { Page } from '../components/Page'
-import { setYear } from '../actions/PageActions'
+import { getPhotos } from '../actions/PageActions'
 
 import '../styles/App.css'
 
 class App extends Component {
   render() {
-    const { user, page, setYearAction } = this.props
+    const { user, page, getPhotosAction } = this.props
 
     return (
       <div className="row">
-        <Page photos={page.photos} year={page.year} setYear={setYearAction} />
+        <Page
+          photos={page.photos}
+          year={page.year}
+          isFetching={page.isFetching}
+          getPhotos={getPhotosAction}
+        />
         <User name={user.name} />
       </div>
     )
@@ -20,7 +25,6 @@ class App extends Component {
 }
 // приклеиваем данные из store
 const mapStateToProps = store => {
-  console.log(store) // посмотрим, что же у нас в store?
   return {
     user: store.user,
     page: store.page,
@@ -29,7 +33,7 @@ const mapStateToProps = store => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    setYearAction: year => dispatch(setYear(year)),
+    getPhotosAction: year => dispatch(getPhotos(year)),
   }
 }
 
