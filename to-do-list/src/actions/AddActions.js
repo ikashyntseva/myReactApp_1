e323922d68ee0e55d9b39440955e467f9a37e357
Irina Kashyntseva Personal <i.kashyntseva@gmail.com>
2019-08-21 12:46:16 +0200

    removed asymc actions

--- to-do-list/src/actions/AddActions.js
+++ to-do-list/src/actions/AddActions.js
@@ -2,19 +2,15 @@ export const TASK_ADD = "TASK_ADD";
 export const TASK_INPUT = "TASK_INPUT";
 
 export function handleAddTask(task) {
-  return dispatch => {
-    dispatch({
-      type: TASK_ADD,
-      payload: task
-    });
+  return {
+    type: TASK_ADD,
+    payload: task
   };
 }
 
 export function inputTask(task) {
-  return dispatch => {
-    dispatch({
-      type: TASK_INPUT,
-      payload: task
-    });
+  return {
+    type: TASK_INPUT,
+    payload: task
   };
 }
