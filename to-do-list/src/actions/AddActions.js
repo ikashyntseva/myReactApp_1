40e63b20c76d449c414785bf26215f007f4bbc3d
Irina Kashyntseva <iryna.kashyntseva@oracle.com>
2019-08-22 13:54:12 +0200

    refactor

--- to-do-list/src/actions/AddActions.js
+++ to-do-list/src/actions/AddActions.js
@@ -1,28 +1,16 @@
-export const TASK_ADD = "TASK_ADD";
-export const TASK_INPUT = "TASK_INPUT";
+export const TASK_ADD = 'TASK_ADD'
+export const TASK_INPUT = 'TASK_INPUT'
 
 export function handleAddTask(task) {
-  /*return dispatch => {
-    dispatch({
-      type: TASK_ADD,
-      payload: task
-    });
-  };*/
   return {
     type: TASK_ADD,
-    payload: task
-  };
+    payload: task,
+  }
 }
 
 export function inputTask(task) {
-  /*return dispatch => {
-    dispatch({
-      type: TASK_INPUT,
-      payload: task
-    });
-  };*/
   return {
     type: TASK_INPUT,
-    payload: task
-  };
+    payload: task,
+  }
 }
