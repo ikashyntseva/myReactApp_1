0e92fa68ac5adcc56b5cb9069f810331c1de0f25
Iris <i.kashyntseva@gmail.com>
2021-05-01 13:42:59 +0200

    Revert "remove react-complete-guide project"
    This reverts commit a6626e2375e6685c6be3cadc35634e88d6886a67.

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+
+class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Max",
+        age: 28
+      },
+      {
+        name: "Manu",
+        age: 29
+      },
+      {
+        name: "Stephanie",
+        age: 26
+      }
+    ]
+  };
+
+  switchNameHandler = () => {
+    //console.log("The button was clicked!");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
